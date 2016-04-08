1327153b04d45807f65af0a7cbb09631d89987a4
hyde2able <hyde31415@gmail.com>
2016-04-08 18:32:07 +0900

    Initial Commit

---
+++ TsuiMana/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TsuiMana
+//
+//  Created by 酒井英伸 on 2016/04/08.
+//  Copyright © 2016年 pokohide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
