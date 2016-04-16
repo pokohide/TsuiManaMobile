5da79129ded0c65072cd70cbdecf6bd758242907
Yasuaki Goto <yasu1003@gmail.com>
2016-04-16 17:55:43 +0900

    2度実行されてるから修正

--- TsuiMana/EvideoViewCell.swift
+++ TsuiMana/EvideoViewCell.swift
@@ -14,37 +14,20 @@ class EvideoViewCell: UITableViewCell {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var word: UILabel!
     
-    var evideo: Evideo? {
-        didSet {
-            // TODO: SDWebImageを使う
-            if let url = evideo?.imageUrl {
-                do {
-                    let imageData = try NSData(contentsOfURL: url, options: NSDataReadingOptions.DataReadingMappedIfSafe)
-                    self.thumbnail.image = UIImage(data: imageData)
-                } catch {
-                    print("Error: can't create image.")
-                }
-            }
-            
-            self.title.text = self.evideo?.title
-            self.word.text = self.evideo?.word
-        }
-    }
-
+    var evideo: Evideo?
 
     override func awakeFromNib() {
-        let url: NSURL = (self.evideo!.imageUrl!)
-        let imageData: NSData
-        do {
-            imageData = try NSData(contentsOfURL: url,options: NSDataReadingOptions.DataReadingMappedIfSafe)
-            self.thumbnail.image = UIImage(data: imageData)
-
-        } catch {
-            print("Error: can't create image.")
-
+        // TODO: SDWebImageを使う
+        if let url = evideo?.imageUrl {
+            do {
+                let imageData = try NSData(contentsOfURL: url, options: NSDataReadingOptions.DataReadingMappedIfSafe)
+                self.thumbnail.image = UIImage(data: imageData)
+            } catch {
+                print("Error: can't create image.")
+            }
         }
 
-        self.title.text = self.evideo!.title
-        self.word.text = self.evideo!.word
+        self.title.text = self.evideo?.title
+        self.word.text = self.evideo?.word
     }
 }
