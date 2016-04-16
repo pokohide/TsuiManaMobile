4f27619a1199397db0f3278c64c6af1259b0f51d
Yasuaki Goto <yasu1003@gmail.com>
2016-04-16 18:11:01 +0900

    とりあえずAPIクライアントの雛形だけ作っておく

---
+++ TsuiMana/WebAPIClient.swift
@@ -0,0 +1,20 @@
+//
+//  WebAPIClient.swift
+//  TsuiMana
+//
+//  Created by 後藤誉昌 on 2016/04/16.
+//  Copyright © 2016年 pokohide. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class WebAPIClient {
+
+    struct Evideos {
+        func getAll() {
+
+        }
+    }
+}
